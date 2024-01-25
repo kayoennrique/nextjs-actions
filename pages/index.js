90553ac99168d9581bbf76d1f138f62efe37313b
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-25 17:26:29 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function HomeScreen() {
   return (
     <div>
       <h1>Página Inicial [nova alteração!]</h1>
-      <NextLink href="/about">
+      <NextLink href="/sobre">
         <a>Ir para a sobre</a>
       </NextLink>
     </div>
