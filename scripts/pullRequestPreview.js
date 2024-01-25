3875594877c7ab4c0673474be8dc01c83262925c
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-25 16:40:46 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -4,10 +4,10 @@ console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf8" });
 const outputLines = output.split("\n");
-const DEPLOY_URL = outputLines[outputLines.lenth - 1];
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END");
 
-console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
+console.log(` : ${DEPLOY_URL}`);
 
 // console.log("[GITHUB_COMMENT]: START");
 // console.log("[GITHUB_COMMENT]: END");
